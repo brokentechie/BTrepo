423a9cf250aa5fa842ceacfa22708f577c56dfab
brokentechie <Andrew.Robbins@BRS-DL-1375-AR.corpnet.liox.org>
2016-01-03 19:30:17 +0000

    Update to wipe

--- plugin.program.btwizard/communitybuilds.py
+++ plugin.program.btwizard/communitybuilds.py
@@ -200,7 +200,7 @@ def Community_Backup():
     backup_zip = xbmc.translatePath(os.path.join(fullbackuppath,title+'.zip'))
     exclude_dirs_full =  ['plugin.program.btwizard']
     exclude_files_full = ["xbmc.log","xbmc.old.log","kodi.log","kodi.old.log",'.DS_Store','.setup_complete','XBMCHelper.conf']
-    exclude_dirs =  ['plugin.program.btwizard','cache', 'system', 'Thumbnails', "peripheral_data",'library','keymaps']
+    exclude_dirs =  ['plugin.program.btwizard','cache', 'system', 'Thumbnails', "peripheral_data",'library']
     exclude_files = ["xbmc.log","xbmc.old.log","kodi.log","kodi.old.log","Textures13.db",'.DS_Store','.setup_complete','XBMCHelper.conf']
     message_header = "Creating full backup of existing build"
     message_header2 = "Creating Universal Build"
--- plugin.program.btwizard/default.py
+++ plugin.program.btwizard/default.py
@@ -83,7 +83,7 @@ tempdbpath   =  xbmc.translatePath(os.path.join(USB,'Database'))
 urlbase      =  'None'
 USER_AGENT = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3'
 base='http://brokentechie.ddns.net/'
-VERSION = "1.0.3"
+VERSION = "2.0.8"
 PATH = "Brokentechie Wizard"
 
 
@@ -182,29 +182,26 @@ def Tools():
 
 #-----------------------------------------------------------------------------------------------------------------
 #Function to clear the addon_data
-def Wipe_Kodi(params):
-    plugintools.log("freshstart.main_list "+repr(params)); yes_pressed=plugintools.message_yes_no(AddonTitle,"Do you wish to restore your","Kodi configuration to default settings?")
-    if yes_pressed:
-        addonPath=xbmcaddon.Addon(id=AddonID).getAddonInfo('path'); addonPath=xbmc.translatePath(addonPath); 
-        xbmcPath=os.path.join(addonPath,"..",".."); xbmcPath=os.path.abspath(xbmcPath); plugintools.log("freshstart.main_list xbmcPath="+xbmcPath); failed=False
-        try:
-            for root, dirs, files in os.walk(xbmcPath,topdown=True):
-                dirs[:] = [d for d in dirs if d not in EXCLUDES]
-                for name in files:
+def Wipe_Kodi():
+    freshstartprompt = xbmcgui.Dialog().yesno('WARNING', 'This will erase all data and reset Kodi to defaults', 'Are you sure you want to continue?', nolabel='No, Cancel',yeslabel='Yes')
+    if freshstartprompt == 1:
+        dp = xbmcgui.DialogProgress()
+        dp.create("Fresh Start" ,"Please Wait...")
+        dp.update(1)
+        homefolder = xbmc.translatePath('special://home/')
+        for root, dirs, files in os.walk(homefolder,topdown=True):
+            dirs[:] = [d for d in dirs if d not in EXCLUDES]
+            for name in files:
                     try: os.remove(os.path.join(root,name))
                     except:
-                        if name not in ["Addons15.db","MyVideos75.db","Textures13.db","xbmc.log"]: failed=True
-                        plugintools.log("Error removing "+root+" "+name)
-                for name in dirs:
+                        pass
+            for name in dirs:
                     try: os.rmdir(os.path.join(root,name))
                     except:
-                        if name not in ["Database","userdata"]: failed=True
-                        plugintools.log("Error removing "+root+" "+name)
-            if not failed: plugintools.log("freshstart.main_list All user files removed, you now have a clean install"); plugintools.message(AddonTitle,"The process is complete, you're now back to a fresh Kodi configuration with Brokentechie Wizard!","Please reboot your system or restart Kodi in order for the changes to be applied.")
-            else: plugintools.log("freshstart.main_list User files partially removed"); plugintools.message(AddonTitle,"The process is complete, you're now back to a fresh Kodi configuration with Brokentechie Wizard","Please reboot your system or restart Kodi in order for the changes to be applied.")
-        except: plugintools.message(AddonTitle,"Problem found","Your settings has not been changed"); import traceback; plugintools.log(traceback.format_exc()); plugintools.log("freshstart.main_list NOT removed")
-        plugintools.add_item(action="",title="Now Exit Kodi",folder=False)
-    else: plugintools.message(AddonTitle,"Your settings","have not been changed"); plugintools.add_item(action="",title="Done",folder=False)
+                        pass
+        dp.close
+        killxbmc()
+
 
 #-----------------------------------------------------------------------------------------------------------------    
 #Maintenance section
@@ -496,6 +493,6 @@ elif mode == 'tools'              : Tools()
 elif mode == 'update'             : addons.Update_Repo()
 elif mode == 'wipetools'          : Wipe_Tools()
 elif mode == 'xbmcversion'        : extras.XBMC_Version(url)
-elif mode == 'wipe_xbmc'          : Wipe_Kodi(params)
+elif mode == 'wipe_xbmc'          : Wipe_Kodi()
 elif mode == 'wizard'            : wizard(name,url,description)
 xbmcplugin.endOfDirectory(int(sys.argv[1]))
