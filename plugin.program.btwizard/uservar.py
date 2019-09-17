385ed3125c8a06e667e1b5bf467800c483bab3c4
brokentechie <gixxerboy76@gmail.com>
2019-09-17 09:59:39 +0100

    Test #2

--- plugin.program.btwizard/uservar.py
+++ plugin.program.btwizard/uservar.py
@@ -1,32 +1,40 @@
 import os, xbmc, xbmcaddon
 
 #########################################################
+### Global Variables ####################################
+#########################################################
+PATH           = xbmcaddon.Addon().getAddonInfo('path')
+ART            = os.path.join(PATH, 'resources', 'art')
+#########################################################
+
+#########################################################
 ### User Edit Variables #################################
 #########################################################
 ADDON_ID       = xbmcaddon.Addon().getAddonInfo('id')
 ADDONTITLE     = 'BrokenTechie Wizard'
-EXCLUDES       = [ADDON_ID]
+BUILDERNAME    = 'BrokenTechie'
+EXCLUDES       = [ADDON_ID, 'repository.brokentechie']
+# Enable/Disable the text file caching with 'Yes' or 'No' and age being how often it rechecks in minutes
+CACHETEXT      = 'Yes'
+CACHEAGE       = 30
 # Text File with build info in it.
 BUILDFILE      = 'http://retrotechie.ddns.net/wizard/brokentechiebuilds.txt'
-# How often you would list it to check for build updates in days
+# How often you would like it to check for build updates in days
 # 0 being every startup of kodi
 UPDATECHECK    = 0
-# Text File with apk info in it.
+# Text File with apk info in it.  Leave as 'http://' to ignore
 APKFILE        = 'http://retrotechie.ddns.net/wizard/apks.txt'
 # Text File with Youtube Videos urls.  Leave as 'http://' to ignore
-YOUTUBETITLE   = 'YouTube Videos'
+YOUTUBETITLE   = ''
 YOUTUBEFILE    = 'http://'
 # Text File for addon installer.  Leave as 'http://' to ignore
 ADDONFILE      = 'http://'
 # Text File for advanced settings.  Leave as 'http://' to ignore
 ADVANCEDFILE   = 'http://'
-
-# Dont need to edit just here for icons stored locally
-PATH           = xbmcaddon.Addon().getAddonInfo('path')
-ART            = os.path.join(PATH, 'resources', 'art')
+#########################################################
 
 #########################################################
-### THEMING MENU ITEMS ##################################
+### Theming Menu Items ##################################
 #########################################################
 # If you want to use locally stored icons the place them in the Resources/Art/
 # folder of the wizard then use os.path.join(ART, 'imagename.png')
@@ -36,6 +44,7 @@ ART            = os.path.join(PATH, 'resources', 'art')
 # Leave as http:// for default icon
 ICONBUILDS     = 'http://'
 ICONMAINT      = 'http://'
+ICONSPEED      = 'http://'
 ICONAPK        = 'http://'
 ICONADDONS     = 'http://'
 ICONYOUTUBE    = 'http://'
@@ -45,7 +54,7 @@ ICONREAL       = 'http://'
 ICONLOGIN      = 'http://'
 ICONCONTACT    = 'http://'
 ICONSETTINGS   = 'http://'
-# Hide the ====== seperators 'Yes' or 'No'
+# Hide the section seperators 'Yes' or 'No'
 HIDESPACERS    = 'No'
 # Character used in seperator
 SPACER         = '='
@@ -69,24 +78,26 @@ THEME5         = '[COLOR '+COLOR1+']Current Theme:[/COLOR] [COLOR '+COLOR2+']%s[
 # Enable 'Contact' menu item 'Yes' hide or 'No' dont hide
 HIDECONTACT    = 'No'
 # You can add \n to do line breaks
-CONTACT        = 'Thank you for choosing BrokenTechie Wizard.'
+CONTACT        = 'Thank you for choosing BrokenTechie Wizard'
 #Images used for the contact window.  http:// for default icon and fanart
 CONTACTICON    = 'http://'
 CONTACTFANART  = 'http://'
 #########################################################
 
 #########################################################
-### AUTO UPDATE #########################################
-########## FOR THOSE WITH NO REPO #######################
+### Auto Update                   #######################
+###        For Those With No Repo #######################
+#########################################################
 # Enable Auto Update 'Yes' or 'No'
 AUTOUPDATE     = 'No'
 # Url to wizard version
-WIZARDFILE     = ''
+WIZARDFILE     = BUILDFILE
 #########################################################
 
 #########################################################
-### AUTO INSTALL ########################################
-########## REPO IF NOT INSTALLED ########################
+### Auto Install                 ########################
+###        Repo If Not Installed ########################
+#########################################################
 # Enable Auto Install 'Yes' or 'No'
 AUTOINSTALL    = 'Yes'
 # Addon ID for the repository
@@ -98,17 +109,21 @@ REPOZIPURL     = 'https://raw.githubusercontent.com/brokentechie/BTrepo/master/z
 #########################################################
 
 #########################################################
-### NOTIFICATION WINDOW##################################
+### Notification Window #################################
 #########################################################
 # Enable Notification screen Yes or No
 ENABLE         = 'No'
 # Url to notification file
-NOTIFICATION   = ''
+NOTIFICATION   = 'http://'
 # Use either 'Text' or 'Image'
 HEADERTYPE     = 'Image'
+# Font size of header
+FONTHEADER     = 'Font14'
 HEADERMESSAGE  = ''
 # url to image if using Image 424x180
-HEADERIMAGE    = ''
+HEADERIMAGE    = 'http://'
+# Font for Notification Window
+FONTSETTINGS   = 'Font13'
 # Background for Notification Window
-BACKGROUND     = ''
+BACKGROUND     = 'http://'
 #########################################################
